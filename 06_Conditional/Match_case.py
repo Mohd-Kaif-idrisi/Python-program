65fc526af90dac67b4d36071af8f1b266937c42b
Mohd-Kaif-idrisi <idrisikaif77@gmail.com>
2025-05-18 01:57:12 +0530

    Else-if program

--- 06_Conditional/Match_case.py
+++ 06_Conditional/Match_case.py
@@ -1,11 +1,11 @@
 a = int(input("Enter number between 1 to 10 "))
 
 match a:
-    case 3:
+    case 1:
         print("You win a charger")
-    case 5:
-        print("You win a phone")
     case 9:
+        print("You win a phone")
+    case 8:
         print("You win a laptop")
     case _:
         print("Better luck,next time")
