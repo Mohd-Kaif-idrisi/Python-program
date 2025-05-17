cc0b6517cabe0b4f4b7be893073b3f9ee9c0d47c
Mohd-Kaif-idrisi <idrisikaif77@gmail.com>
2025-05-18 01:53:37 +0530

    User input programs

--- 03_User_input/ArthmeticOp.py
+++ 03_User_input/ArthmeticOp.py
@@ -1,10 +1,10 @@
-a = int(input("Enter your first number "))
-b = int(input("Enter your second number "))
+first = int(input("Enter your first number "))
+second = int(input("Enter your second number "))
 
-print("A + B is : ",a+b)
-print("A - B is : ",a-b)
-print("A * B is :",a*b)
-print("A / B is :",a/b)
+print("A + B is : ",first+second)
+print("A - B is : ",first-second)
+print("A * B is :",first*second)
+print("A / B is :",first/second)
 
 
 
