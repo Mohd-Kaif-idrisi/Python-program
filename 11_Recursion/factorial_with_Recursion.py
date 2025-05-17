09d3886628dd3a8c597bd536166819473864f197
Mohd-Kaif-idrisi <idrisikaif77@gmail.com>
2025-05-17 15:50:02 +0530

    Recursion programs

--- 11_Recursion/factorial_with_Recursion.py
+++ 11_Recursion/factorial_with_Recursion.py
@@ -1,8 +1,11 @@
+# calling a function by function itself is called recursion,
+# It is work like a loop and we have to call a function in the return in a function
+
 def factorial(n):
     if(n == 0 or n == 1):
-        return 1
+        return n
     else:
         return n * factorial(n-1)
     
 
-print(factorial(4))    
+print(factorial(5))    
