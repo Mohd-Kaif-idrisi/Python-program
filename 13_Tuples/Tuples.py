3f1cb5af499ba6143d05f9d87de2594b41818294
Mohd-Kaif-idrisi <idrisikaif77@gmail.com>
2025-05-18 23:09:00 +0530

    Tuples code

---
+++ 13_Tuples/Tuples.py
@@ -0,0 +1,10 @@
+# tuple are immutable that mean you can not
+# change it, we you tuple becuse sometime we dont want
+# to change our value,
+# tuples are much faster then list
+
+tu = (45, 54, 65, 76)
+
+print(tu)
+
+print(tu[1])
---
+++ 13_Tuples/tuples_method.py
@@ -0,0 +1,7 @@
+tu = (45, 67, 23, 23)
+
+# count karega kitni baar 23 aa rha h
+print(tu.count(23))
+
+# count index value
+print(tu.index(23))
