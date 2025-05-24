0478bf55ddd146fc4fe56fefcb0d27494ecf6bba
Mohd-Kaif-idrisi <idrisikaif77@gmail.com>
2025-05-24 21:10:27 +0530

    sets code

--- 14_sets/Sets_opretion.py
+++ 14_sets/Sets_opretion.py
@@ -2,4 +2,6 @@ sets1 = {54, 67, 32, 78}
 sets2 = {1,2,3,4}
 
 
-print(sets1.union(sets2))
+print(sets1.union(sets2))  #combain 2 sets
+
+print(sets1.intersection(sets2))  #only print repeated number from both set
