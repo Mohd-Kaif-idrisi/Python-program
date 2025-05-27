bbd90143992b73386d16f7be7ad5bb0b22a1054c
Mohd-Kaif-idrisi <idrisikaif77@gmail.com>
2025-05-27 20:02:14 +0530

    Dictionary code

--- 15_Dictionary/dictionary.py
+++
@@ -1,8 +0,0 @@
-dic = {"Kaif": 45, "Irfan": 42, "Mohit": 62}
-
-print(dic)
-
-print(dic["Irfan"])
-
-dic["Kaif"] = 99
-print(dic)
--- 15_Dictionary/dictionary_method.py
+++
@@ -1,8 +0,0 @@
-dic = {"Kaif": 45, "Irfan": 42, "Mohit": 62}
-
-print(dic.keys())
-
-print(dic.values())
-
-dic.pop("Mohit")
-print(dic)
