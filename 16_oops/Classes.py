33b5424da4390d884ff213b72381dec74032bf0b
Mohd-Kaif-idrisi <idrisikaif77@gmail.com>
2025-05-28 22:55:17 +0530

    oops code

---
+++ 16_oops/Classes.py
@@ -0,0 +1,29 @@
+class result:
+    name = "irfan"
+    marks = 280
+    subject = "science"
+    def marksheet(self):
+
+        print(f"{self.name} get {self.marks} marks out of 500 in {self.subject}")
+
+a = result()
+b = result()
+c = result()       
+
+b.name = "kaif"
+b.marks = 400
+
+
+c.name = "mohit"
+c.marks = 500
+"science"
+a.marksheet()
+b.marksheet()
+c.marksheet()
+
+ 
+
+
+
+
+ 
