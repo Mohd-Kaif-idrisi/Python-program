561446e047e0dc107647cad09275969e74c5a544
Mohd-Kaif-idrisi <idrisikaif77@gmail.com>
2025-06-14 11:44:51 +0530

    drink water remainder app

--- DRINK-WATER-REMAINDER/main.py
+++ DRINK-WATER-REMAINDER/main.py
@@ -1,9 +1,12 @@
 import time
 from notifypy import Notify
+
 notification = Notify()
+
 while True:
     print("Drink your water")
     notification.title = "Drink water remainder"
     notification.message = "Please sip some water"
     notification.send()
+    
     time.sleep(3)    
